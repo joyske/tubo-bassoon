ae79a0328d234f40488dd69e0f504ad9e9c46874
lettiChop <38751506+lettiChop@users.noreply.github.com>
2018-06-06 14:24:16 +0200

    Add files via upload

---
+++ a3_b.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+// Zusätzliche Header-Dateien hier!
+#include <unistd.h>
+
+#include "vorgabe.h"
+
+void *work(void *arg)
+{
+	int my_num = *((int *)arg);
+	int value; 
+	
+	printf("[Ch%d] Ich bin bereit zu arbeiten!\n", my_num);
+
+	// *** HIER EUER CODE ***
+	chemical_t *ingredients[RECIPE_SIZE];
+	for (int i = 0; i < RECIPES_PER_CHEMIST; i++)
+	  { 
+	    //Wir generieren uns ein Rezept mit 4 Zutaten 
+	    get_recipe(ingredients);
+
+	    //dann gehen wir die Zutatenliste durch
+	    for (int j = 0; j < RECIPE_SIZE; j++)
+	      {
+		//...und holen uns unsere Zutaten eine nach der anderen
+		//jedes Mal wenn eine Chemikalienflasche geholt wird 
+		//muss sem (die Anzahl der verbleibenen Flaschen im Regal)
+		//dekrementiert werden da ja eine Flasche weniger
+		//zur Verfuegung steht 
+		sem_wait(&(*ingredients[j]).sem);
+		sem_getvalue(&(*ingredients[j]).sem, &value); 
+		printf("[Ch%d] ich nehme mir die Chemikalie %s! Es sind jetzt noch %d Flaschen davon vorhanden!\n", my_num, (*ingredients[j]).name, value);
+				
+	      }
+
+	    for (int v = 0; v < RECIPE_SIZE; v++) 
+	      {
+		//dann verarbeiten wir die Chemikalien nacheinander
+	        sleep((*ingredients[v]).time_needed);
+		printf("[Ch%d] %s wird von mir verarbeitet... \n", my_num, (*ingredients[v]).name);
+	      }
+
+	    //erst nachdem wir ALLE in unserem Rezept benoetigten Chemikalien 
+	    //verarbeitet haben
+	    for (int r = 0; r < RECIPE_SIZE; r++) 
+	      {
+		//...werden die Chemikalienflaschen wieder zurueck gebracht 
+		//sem wird freigegeben bzw. incrementiert 
+		//da die Chemikalie ja wieder fuer andere Chemiker verfuegbar ist
+		sem_post(&(*ingredients[r]).sem);
+		printf("[Ch%d] %s wird jetzt von mir zurück gebracht! \n", my_num, (*ingredients[r]).name);
+	      }
+	  }
+}
