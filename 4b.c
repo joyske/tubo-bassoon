096984eb37667a008b54c202ebc0f5fa0ce68a4d
lettiChop <38751506+lettiChop@users.noreply.github.com>
2018-06-21 00:17:29 +0200

    Update 4b.c

--- 4b.c
+++ 4b.c
@@ -10,39 +10,50 @@ void nf_free(void *addr)
 	size_t j = address-1;
 	size_t zero = 0; 
 	
-	for (size_t i = address; i <= range; i++) 
+	if (addr != NULL) 
 	{
-		if (i < range) 
+		if (to_free > zero && allocation_list[address].status == CHUNK_ALLOCATED)
 		{
-			allocation_list[i].status = CHUNK_FREE; 
-		}
-		else if (i == range && allocation_list[range].status == CHUNK_FREE) 
-		{
-			to_attach = allocation_list[range].length; 
-			allocation_list[address].length = to_free+to_attach;
-			allocation_list[range].length = 0;
-		}  
-	}
-	
-	while (j >= zero) 
-	{
-		if (allocation_list[j].status == CHUNK_FREE)
-		{
-			if (j > zero && allocation_list[j].length != 0)
+			for (size_t i = address; i <= range; i++) 
 			{
-				j--; 
+				if (i < range) 
+				{
+					allocation_list[i].status = CHUNK_FREE; 
+				}
+				else if (i == range && allocation_list[range].status == CHUNK_FREE) 
+				{
+					to_attach = allocation_list[range].length; 
+					allocation_list[address].length = to_free+to_attach;
+					allocation_list[range].length = 0;
+				}  
 			}
-			else if (allocation_list[j].length > 0)
+	
+			while (j >= zero) 
 			{
-				to_attach = allocation_list[j].length; 
-				allocation_list[j].length = allocation_list[address].length+to_attach; 
-				allocation_list[address].length = 0; 
-				break;  
-			} 
-		}  
+				if (allocation_list[j].status == CHUNK_FREE)
+				{
+					if (j > zero && allocation_list[j].length == 0)
+					{
+						j--; 
+					}
+					else if (allocation_list[j].length > 0)
+					{
+						to_attach = allocation_list[j].length; 
+						allocation_list[j].length = allocation_list[address].length+to_attach; 
+						allocation_list[address].length = 0; 
+						break;  
+					} 
+				}  
 	 
-	}  
-	
-	
+			}
+			
+		}
+		else 
+		{
+			printf("\n [!] FEHLER! Ungültige Adresse!");
+			return 255;
+		}
+		
+	} 
 	
 }
